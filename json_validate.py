d39fd88b86a000eda2bbe8d91b2c9dc12e9cf635
d <dingsong87@gmail.com>
2017-07-12 11:50:44 +0800

    json_validate.py

---
+++ json_validate.py
@@ -0,0 +1,26 @@
+schema_users_post = {
+    "$schema": "http://json-schema.org/schema#",
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "password": {"type": "string"}
+    },
+    "required": ["name", "password"],
+    "additionalProperties": False
+}
+
+schema_sessions_post = {
+    "$schema": "http://json-schema.org/schema#",
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "password": {"type": "string"}
+    },
+    "required": ["name", "password"],
+    "additionalProperties": False
+}
+
+SCHEMA = {
+    'schema_users_post': schema_users_post,
+    'schema_sessions_post': schema_sessions_post
+}
